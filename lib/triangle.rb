2714b7a7695b4028bcf2e36264b95e6312d3a111
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-26 19:09:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,13 @@ class Triangle
   end
   
   def kind
-    if (a > 0 && b > 0 && c > 0) && (a == b && b == c && c == a)
+    if a <= 0 || b <= 0 || c <= 0
+      
+      
+      
+      
+      
+    elsif a == b && b == c && c == a
       :equilateral
     elsif a == b || b == c || c == a
       :isosceles
